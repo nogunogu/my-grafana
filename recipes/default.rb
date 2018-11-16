dc6f1c8fedc16c47dbff93d9cf3e60f14eaf07e3
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-11-16 22:48:09 +0900

    install grafana

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_repository 'grafana' do
+  uri 'https://packagecloud.io/grafana/stable/debian/'
+  components ['main']
+  distribution 'stretch'
+  action :add
+  key 'https://packagecloud.io/gpg.key'
+end
+
+apt_update
+
+apt_package 'grafana'
+
+service 'grafana-server' do
+  action [:enable, :start]
+end
